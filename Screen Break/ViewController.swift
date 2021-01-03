fc29d83a09afd0094c75aabf39d4ba37a681a6c6
SaeedBaig <amateuracademic616@gmail.com>
2021-01-03 21:45:02 +1100

    Added stopwatch functionality
    The app counts up (but only when the app is in the foreground).

--- Screen Break/ViewController.swift
+++ Screen Break/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /* UI elements */
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var timerText: UILabel!
+    
+    /* Global variables */
+    var timer = Timer()
+    var isTimerRunning = false
+    var numSeconds = 0     // just use seconds for now
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resetButton.isEnabled = false
+        pauseButton.isEnabled = false
+        playButton.isEnabled = true
     }
-
-
+    
+    /* On button tap */
+    
+    // Reset
+    @IBAction func onTapResetButton(_ sender: Any) {
+        print("You pressed Reset button")
+    }
+    
+    // Play
+    @IBAction func onTapPlayButton(_ sender: Any) {
+        if !isTimerRunning {
+            timer = Timer.scheduledTimer(
+                timeInterval: 1,    // Call timer every 1 second
+                target: self,
+                selector: #selector(runTimer),
+                userInfo: nil,
+                repeats: true
+            )
+            isTimerRunning = true
+            
+            resetButton.isEnabled = true
+            pauseButton.isEnabled = true
+            playButton.isEnabled = false
+        }
+    }
+    
+    // Pause
+    @IBAction func onTapPauseButton(_ sender: Any) {
+        print("You pressed Pause button")
+    }
+    
+    /* Helper functions */
+    @objc func runTimer() {
+        numSeconds += 1
+        timerText.text = "\(numSeconds) seconds"
+    }
 }
 
